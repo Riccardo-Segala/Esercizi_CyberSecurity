161149f52516364a9d2d2df4c8cbe0cde8cea38f
Riccardo Segala <riccardo.segala@edu.unife.it>
2025-03-11 09:36:36 +0100

    Add files via upload

---
+++ Esercizio2.py
@@ -0,0 +1,33 @@
+def score_word(word, available):
+    """
+    Compute the score for an acceptable word
+
+	word : word to be scored
+	available : string of the available letters
+    
+    >>> score_word('card', 'ACDLORT')
+    1
+    >>> score_word('color', 'ACDLORT')
+    5
+    >>> score_word('cartload', 'ACDLORT')
+    15
+    """
+    points = 0
+    if len(word)== 4:
+        points += 1
+    elif len(word) >4:
+        points += len(word)
+        lista = []
+        for w in word:
+            for a in available:
+                if w.lower() == a.lower():
+                    if (w.lower() in lista) == False:
+                        lista.append(w)
+        
+        if len(lista) == len(available):
+            points += 7
+    return points
+
+if __name__ == "__main__":
+    import doctest
+doctest.testmod()
---
+++ Esercizio3.py
@@ -0,0 +1,33 @@
+def clean_book(source_filename, destination_filename):
+    """
+    Remove everything except the book itself. Return the number of lines of the cleaned book
+
+    source_filename : where to find the book to be cleaned
+    destination_filename : where to store the cleaned book
+
+    >>> clean_book("stevenson.txt", "stevenson_clean.txt")
+    2530
+    """
+
+    f_source = open(source_filename)
+    f_destination = open(destination_filename,"w")
+
+    start = False
+    end = False
+    total_line = 0
+
+    for line in f_source:
+        if line[0:len(line)-1] == "*** END OF THIS PROJECT GUTENBERG EBOOK THE STRANGE CASE OF DR. ***":
+            end = True
+        if start == True and end == False:
+            f_destination.write(line)
+            total_line+=1
+        if line[0:len(line)-1] == "*** START OF THIS PROJECT GUTENBERG EBOOK THE STRANGE CASE OF DR. ***":
+            start = True
+        
+    return total_line
+
+
+if __name__ == "__main__":
+    import doctest
+doctest.testmod()
